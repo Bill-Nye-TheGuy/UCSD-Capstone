f9639c1228f637f528de45d4546dab1f4275db21
Bill-Nye-TheGuy <83429145+Bill-Nye-TheGuy@users.noreply.github.com>
2022-12-21 13:19:10 -0700

    Updated
    Added production code.

---
+++ 24.6/24.6.1/EndToEndRefactor.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 10:47:54 2022
+
+@author: darth
+"""
+
+#%% Setup
+import warnings
+warnings.filterwarnings("ignore")
+
+import joblib
+from pydub import AudioSegment
+import librosa
+import numpy as np
+import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'} # Turn off uneccsary warnings
+
+import tensorflow as tf
+import argparse
+
+#%% Setup CMD argument parsing
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="Path to MP3")
+args = parser.parse_args()
+
+#%% Import Pipeline Objects and Models
+LabelEncode = joblib.load("LabelEncoder.jblb")
+TFEncode = joblib.load("TFEncoder.jblb")
+TFScale = joblib.load("TFScaler.jblb")
+XBGModel = joblib.load("XGBTuned.jblb")
+TFModel = tf.keras.models.load_model('DL.h5')
+
+#%% Create Object
+class ModelAssets:
+
+    #%% Data Feature Pipeline
+    def feature_extraction(self,filename, accent = None, sampling_rate=48000):
+        #path = 'cv-valid-'+folder+'/'+filename
+        path = filename
+        features = list()
+        sound = AudioSegment.from_file(path) # Load Audio File
+        audio = np.array(sound.get_array_of_samples(),dtype=np.float32)
+        #Calculate Features
+        spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sampling_rate))
+        spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=audio, sr=sampling_rate))
+        spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(y=audio, sr=sampling_rate))
+        features.append(accent)
+        features.append(spectral_centroid)
+        features.append(spectral_bandwidth)
+        features.append(spectral_rolloff)
+        mfcc = librosa.feature.mfcc(y=audio, sr=sampling_rate,n_mfcc = 12, lifter = 5)
+        #Convert MFCC's to mean and standard deviation to reduce dimensions
+        for el in mfcc:
+            features.append(np.mean(el))
+            features.append(np.std(el))
+        dataset = []
+        dataset.append(features)
+        #Setup Feature dataframe
+        S_df = pd.DataFrame(dataset,columns = ["label", "spectral_centroid", "spectral_bandwidth", "spectral_rolloff",
+                                            "mfcc1", "Std1",
+                                            "mfcc2", "Std2",
+                                            "mfcc3", "Std3",
+                                            "mfcc4", "Std4",
+                                            "mfcc5", "Std5",
+                                            "mfcc6", "Std6",
+                                            "mfcc7", "Std7",
+                                            "mfcc8", "Std8",
+                                            "mfcc9", "Std9",
+                                            "mfcc10", "Std10",
+                                            "mfcc11", "Std11",
+                                            "mfcc12", "Std12",
+                                            ])
+        S_df.drop('label',axis = 1,inplace = True) # Drop the label column, not needed for predictions
+        return S_df
+    
+    #%% Predict Using XGBoost
+    def predictWithXGB(self,features):
+        prediction = XBGModel.predict(features)
+        print(str(LabelEncode.classes_[prediction]))
+        
+    #%% Predict using DL
+    def predictWithDL(self,features):
+        prediction = TFModel.predict(TFScale.transform(features.values)).argmax(1)
+        print(str(LabelEncode.classes_[prediction]))
+        
+    #%% Predict on a file with both algos
+    def predictOnFile(self,Filename):
+        f = self.feature_extraction(Filename)
+        prediction = XBGModel.predict(f)
+        print('XGBoost: ' + str(LabelEncode.classes_[prediction]))
+        prediction = TFModel.predict(TFScale.transform(f.values),verbose = 0).argmax(1)
+        print('Deep Learning: ' + str(LabelEncode.classes_[prediction]))
+        
+#%% CMD Flags
+if(args.file == None):
+    pass
+else:
+    print("Preicting on: " + args.file)
+    ModelAssets().predictOnFile(args.file)
